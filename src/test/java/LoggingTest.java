892a2d7488b1e82f68fdce802f7087456ffe2e98
Vadim Lizogub <vadlvadl789@gmail.com>
2018-09-10 20:05:55 +0300

    Test class renamed to LoggingTest

--- src/test/java/LoggingTest.java
+++ src/test/java/LoggingTest.java
@@ -2,21 +2,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class SampleTest1 {
+public class LoggingTest {
 
     private static final Logger LOGGER =
-            LogManager.getLogger(SampleTest1.class.getName());
+            LogManager.getLogger(LoggingTest.class.getName());
 
     @Test
-    public void TestCase1() {
+    public void generateLogTest() {
         LOGGER.debug("Some Test - debug message");
         LOGGER.info("Some Test - info message");
         LOGGER.error("Some Test - error message");
     }
 
-    @Test
-    public void TestCase2() {
-        LOGGER.info(SampleTest1.class.getCanonicalName());
-        LOGGER.fatal("TestCase2 - Fatal message");
-    }
 }
